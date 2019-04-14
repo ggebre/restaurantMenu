00a9958f0965ffde3985bc2d6acef9092f31d397
Getu Gebre <lavumom@yahoo.co.uk>
2019-04-14 16:07:19 -0500

    derailment created

--- Restaurant/MenuItemDetailViewController.swift
+++ Restaurant/MenuItemDetailViewController.swift
@@ -10,13 +10,37 @@ import UIKit
 
 class MenuItemDetailViewController: UIViewController {
     
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var detailTextLabel: UILabel!
+    
+    @IBOutlet weak var addToOrderButton: UIButton!
+    
     var menuItem: MenuItem!
+    
+    
+    @IBAction func addToOrderButtonTapped(_ sender: Any) {
+        UIView.animate(withDuration: 0.3) {
+            self.addToOrderButton.transform = CGAffineTransform(scaleX: 3.0, y: 3.0)
+            self.addToOrderButton.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = menuItem.category
+        addToOrderButton.layer.cornerRadius = 5.0
+        updateUI()
         // Do any additional setup after loading the view.
     }
     
+    func updateUI(){
+        titleLabel.text = menuItem.name
+        priceLabel.text = String(format: "$%.2f", menuItem.price)
+        detailTextLabel.text = menuItem.detailText
+    }
 
     /*
     // MARK: - Navigation
